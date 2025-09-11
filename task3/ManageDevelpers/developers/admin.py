b720828a47eb02cb23e04cabf1d63baa3d4a36f3
ma131812 <m.a.131812005@gmail.com>
2025-09-11 16:30:39 +0330

    feal : write models , added to admin panel ,add app in setting.installedapp

--- task3/ManageDevelpers/developers/admin.py
+++ task3/ManageDevelpers/developers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(DevelopersModel)
+class DevelopersAdmin(admin.ModelAdmin):
+    list_display=[
+        "first_name",
+        "last_name",
+        "email",
+        "age"
+    ]
+
+@admin.register(ProjectModel)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display=[
+        "title"
+    ]
+
+@admin.register(SkillsModel)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display=[
+        "title"
+    ]
--- task3/ManageDevelpers/developers/models.py
+++ task3/ManageDevelpers/developers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class DevelopersModel(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=75)
+    email=models.EmailField()
+    age=models.PositiveIntegerField()
+    projects=models.ManyToManyField("ProjectModel",related_name="developers")
+
+    class Meta:
+        ordering=['last_name']
+
+    def __str__(self):
+        return self.first_name + self.last_name
+    
+class SkillsModel(models.Model):
+    title=models.CharField(max_length=30)
+    desciption=models.TextField()
+    developer=models.ForeignKey("DevelopersModel",on_delete=models.CASCADE,related_name="skills",)
+
+    def __str__(self):
+        return self.title
+
+class ProjectModel(models.Model):
+    title=models.CharField(max_length=30)
+    desciption=models.TextField()
+
+    def __str__(self):
+        return self.title
